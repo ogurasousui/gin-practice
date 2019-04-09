c5f4da7e4467fd6df5266b695500d840c27498ac
ogurasousui <ogurasousui2@gmail.com>
2019-04-09 16:16:13 +0900

    DB接続用のメソッド追加

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+	"app/db"
 	"app/model"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
-	db, err := gorm.Open("mysql", "root:@tcp(db:3306)/gin_app?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		panic("failed to connect database")
-	}
+	db := db.Connection()
 	defer db.Close()
 
 	db.AutoMigrate(&model.User{})
